ad0c016cc77f60ce31edafffbd3860803c04919f
amin-ep <amincode24@gmail.com>
2025-08-19 14:14:32 +0330

    change some layouts

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "./components/LoginForm";
 
 export default function page() {
   return (
-    <div>
+    <div className="flex min-h-screen items-center justify-center py-10">
       <div className="container mx-auto grid max-w-240 grid-cols-1 gap-4 rounded-xl px-2 lg:grid-cols-2 lg:gap-0">
         <div className="flex items-center justify-center rounded-xl bg-white py-4 md:py-8 lg:rounded-l-xl lg:rounded-r-none">
           <SignupForm />
