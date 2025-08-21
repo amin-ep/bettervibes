9e60695eae9af0340d2e3ba7099e3b577abbb254
amin-ep <amincode24@gmail.com>
2025-08-21 15:14:09 +0330

    update verify page background

--- app/(authentication)/verify/page.tsx
+++ app/(authentication)/verify/page.tsx
@@ -2,7 +2,7 @@ import VerificationForm from "./components/VerificationForm";
 
 function VerifyPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center py-10">
+    <div className="from-primary to-error-content flex min-h-screen items-center justify-center bg-gradient-to-br py-10">
       <div className="rounded-xl bg-white p-2 md:p-4 lg:p-6 xl:p-8">
         <VerificationForm />
       </div>
