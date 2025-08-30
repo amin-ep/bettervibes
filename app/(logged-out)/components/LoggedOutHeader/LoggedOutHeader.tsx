cf087d3a4572db4b24c3bf3a138fce5b33ad0ab5
amin-ep <amincode24@gmail.com>
2025-08-30 15:30:54 +0330

    fix: change (logout

--- app/(logged-out)/components/LoggedOutHeader/LoggedOutHeader.tsx
+++ app/(logged-out)/components/LoggedOutHeader/LoggedOutHeader.tsx
@@ -6,12 +6,18 @@ export default function LoggedOutHeader() {
   return (
     <header className="flex items-center justify-between">
       <LogoLink variation="horizontal" />
-      <nav>
-        <Link
+      <nav className="flex items-center gap-2">
+        {/* <Link
           href="/auth"
           className="btn btn-primary !px-2 text-xs md:!px-6 md:text-sm"
         >
           Signin | Signup
+        </Link> */}
+        <Link href="/login" className="btn btn-ghost">
+          Login
+        </Link>
+        <Link href="/signup" className="btn btn-primary">
+          Signup
         </Link>
       </nav>
     </header>
