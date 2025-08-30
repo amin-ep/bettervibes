e8088755f51c8fcd49ce9d8dfb7cbf6e4f38f780
amin-ep <amincode24@gmail.com>
2025-08-30 15:37:57 +0330

    fix: remove unused comment

--- app/(logged-out)/components/LoggedOutHeader/LoggedOutHeader.tsx
+++ app/(logged-out)/components/LoggedOutHeader/LoggedOutHeader.tsx
@@ -7,12 +7,6 @@ export default function LoggedOutHeader() {
     <header className="flex items-center justify-between">
       <LogoLink variation="horizontal" />
       <nav className="flex items-center gap-2">
-        {/* <Link
-          href="/auth"
-          className="btn btn-primary !px-2 text-xs md:!px-6 md:text-sm"
-        >
-          Signin | Signup
-        </Link> */}
         <Link href="/login" className="btn btn-ghost">
           Login
         </Link>
