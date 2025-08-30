637209c4374904374c040a364fd7436c9be3c07f
amin-ep <amincode24@gmail.com>
2025-08-30 10:56:56 +0330

    fix: fix ForgetPasswor page layout

--- app/(authentication)/forget-password/page.tsx
+++ app/(authentication)/forget-password/page.tsx
@@ -2,11 +2,5 @@ import React from "react";
 import ForgetPasswordForm from "./components/ForgetPasswordForm";
 
 export default function ForgetPasswordPage() {
-  return (
-    <div className="from-primary to-error-content flex min-h-screen items-center justify-center bg-gradient-to-br py-10">
-      <div className="rounded-xl bg-white p-2 md:p-4 lg:p-6 xl:p-8">
-        <ForgetPasswordForm />
-      </div>
-    </div>
-  );
+  return <ForgetPasswordForm />;
 }
